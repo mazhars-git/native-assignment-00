cf765b79eb9d20eed3dd0989456067e17615e97b
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-06-28 23:51:25 +0600

    item screen

--- src/components/screens/item.js
+++ src/components/screens/item.js
@@ -1,10 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground, StyleSheet } from 'react-native'
 import React from 'react'
+import { colors } from './../theme/colors';
 
 export default function Item() {
   return (
-    <View>
-      <Text>Item</Text>
+    <View style= {styles.container}>
+        <ImageBackground source={require("../../../image/bgpic.png")} 
+            style={{height: '100%', width: '100%', marginBottom: 150}}
+            resizeMode="cover"
+        />
+        <View style={styles.itemContentBox}>
+            <Text style={styles.title}>Boston Lettuce</Text>
+            <Text style={styles.title}>Boston Lettuce</Text>
+            <Text style={styles.title}>Boston Lettuce</Text>
+            <Text style={styles.title}>Boston Lettuce</Text>
+        </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {},
+    itemContentBox: {
+        backgroundColor: colors.bg,
+        padding: 20,
+
+    },
+    title: {},
+    container: {},
+})
