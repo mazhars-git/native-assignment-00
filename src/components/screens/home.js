c3c0809fd9c3c0120535f0b40730011e99eeb000
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-06-28 23:05:51 +0600

    update

--- src/components/screens/home.js
+++ src/components/screens/home.js
@@ -1,28 +1,46 @@
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, Text, StyleSheet, Image, Pressable } from 'react-native'
 import React from 'react'
 import { colors } from './../theme/colors';
-import { Pressable } from 'react-native-web';
+import Header from './header';
 
 export default function Home() {
   return (
     <View style={styles.container}>
-        
-        <View style={styles.contentArea}>
-            <Image style={{height: 100, width: 100, marginTop: 20}} source = {require('../../../image/logo-2.png')} />
-            <Text style={{fontSize: 30, fontWeight: '500', marginBottom: 25, color: colors.text}}>Non-Contact <br/> Deliveries</Text>
-            <Text style={{width: '80%', fontSize: 20, marginBottom: 25}}>When placing an order, select the option “Contactless delivery” and the courier will leave your order at the door.</Text>
+        <Header />
+        <View style={styles.contentBox}>
+            
+            <View style={styles.contentArea}>
+                <Image style={{height: 100, width: 100, marginTop: 20}} source = {require('../../../image/logo-2.png')} />
+                <Text style={{
+                    fontSize: 30, 
+                    fontWeight: '500', 
+                    marginBottom: 25, 
+                    color: colors.text
+                }}>Non-Contact <br/> Deliveries</Text>
+
+                <Text style={{
+                    width: '80%', 
+                    fontSize: 20, 
+                    marginBottom: 25
+                }}>When placing an order, select the option “Contactless delivery” and the courier will leave your order at the door.</Text>
+            </View>
+
+            <Pressable onPress ={ () => {
+                }} style = {styles.btn}>
+                <Text style={{
+                        color: 'white', 
+                        fontSize: 20, 
+                        fontWeight: 'bold'
+                    }}>Order Now</Text>
+            </Pressable>
+
+            <Pressable style = {styles.btn2}>
+                <Text style={{
+                    fontSize: 20, 
+                    fontWeight: 'bold'
+                }}>Dismiss</Text>
+            </Pressable>
         </View>
-
-        <Pressable onPress ={ () => {
-
-            }} style = {styles.btn}>
-            <Text style={{color: 'white', fontSize: 20, fontWeight: 'bold'}}>Order Now</Text>
-        </Pressable>
-
-        <Pressable style = {styles.btn2}>
-            <Text style={{fontSize: 20, fontWeight: 'bold'}}>Dismiss</Text>
-        </Pressable>
-
     </View>
   )
 }
@@ -30,6 +48,9 @@ export default function Home() {
 
 const styles = StyleSheet.create({
     container: {
+        backgroundColor: colors.blue
+    },
+    contentBox: {
         backgroundColor: colors.bg,
         borderTopLeftRadius: 30,
         borderTopRightRadius: 30,
